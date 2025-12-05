0bedd5b10af8752991728c32bfb56337579fbbf6
Aakshat Hariharan <163146641+Aaks-hatH@users.noreply.github.com>
2025-12-04 21:28:01 -0500

    Update HeroSection.jsx

--- src/components/portfolio/HeroSection.jsx
+++ src/components/portfolio/HeroSection.jsx
@@ -119,7 +119,7 @@ export default function HeroSection() {
             {adminStatus.online && <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>}
             <span className={`relative inline-flex rounded-full h-2 w-2 ${adminStatus.online ? 'bg-green-500' : 'bg-slate-600'}`}></span>
           </div>
-          {adminStatus.online ? "Aakshat ONLINE" : "Aakshat OFFLINE"}
+          {adminStatus.online ? "I AM ONLINE" : "I AM OFFLINE"}
         </div>
         
         {adminStatus.online && (
